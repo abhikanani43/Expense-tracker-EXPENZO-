54ff8a80f15488c4dd7770df012e0dcf03325698
abhikanani43 <141323262+abhikanani43@users.noreply.github.com>
2025-01-24 17:23:27 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ExpenseTracker from './components/ExpenseTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseTracker />
+    </div>
+  );
+}
+
+export default App;
